91e307acab15588cc6701ea6a46c7ae175cb8a2b
Papizola <120310298+Papi64s@users.noreply.github.com>
2024-05-09 03:43:26 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,68 +1,157 @@
-#!/bin/bash
-#
-# Compile script for kernel
-# Copyright (C) 2022 Craft Rom.
+#! /bin/bash
 
-SECONDS=0 # builtin bash timer
+ # Script For Building Android arm64 Kernel
+ #
+ # Copyright (c) 2018-2020 Panchajanya1999 <rsk52959@gmail.com>
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #      http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
 
-#Set Color
-blue='\033[0;34m'
-grn='\033[0;32m'
-yellow='\033[0;33m'
-red='\033[0;31m'
-nocol='\033[0m'
-txtbld=$(tput bold)
-txtrst=$(tput sgr0) 
+#Kernel building script
 
-DATE=$(date '+%Y-%m-%d  %H:%M')
-HOME_DIR="JFla"
-
-# Telegram setup
-push_message() {
-    curl -s -X POST \
-         https://api.telegram.org/bot5759949181:AAH-yOhz9OdUsFeb-82v92x5sy-o5DNV3UI/sendMessage \
-        -d chat_id="--1001590833950" \
-        -d text="$1" \
-        -d "parse_mode=html" \
-        -d "disable_web_page_preview=true"
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
 }
 
-push_document() {
-    curl -s -X POST \
-         https://api.telegram.org/bot5759949181:AAH-yOhz9OdUsFeb-82v92x5sy-o5DNV3UI/sendDocument \
-        -F chat_id="-100159083395" \
-        -F document=@"$1" \
-        -F caption="$2" \
-        -F "parse_mode=html" \
-        -F "disable_web_page_preview=true"
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+    exit 1
 }
 
-push_message "<b>Build bot is running.</b>
-<b>Date:</b> <code>$DATE</code>"
-cd $HOME/$HOME_DIR
+##------------------------------------------------------##
+##----------Basic Informations, COMPULSORY--------------##
 
-for device in onclite ; do
+# The defult directory where the kernel should be placed
+KERNEL_DIR=$PWD
 
-    mkdir -p $HOME/$HOME_DIR/JFla/$device
-    
-(
-	echo -e "$blue    \nStarting kernel compilation for $device\n $nocol"
-	LOG="build-$device.log"
-	BUILD_DATE=$(date '+%Y-%m-%d  %H:%M')
-	# Push message if build started
-	push_message "- Start building kernel for <b><code>$device</code></b>
-	<b>BuildDate:</b> <code>$BUILD_DATE</code>"
-	cd $HOME/$HOME_DIR/JFla-Karamel/$device 
-	bash build.sh -n | tee $LOG
-		
-	echo -e "$blue --- Uploading to SourceForge *.zip. $nocol"
-	push_message "- Start uploading to SourceForge <i>*.zip</i> from <b><code>$device</code></b>"
-	scp $HOME/$HOME_DIR/chidori/$device/*-signed.zip Risti699@frs.sourceforge.net:/home/frs/project/jfla-test-tmp/JFla-Test
-	
-	push_document "$LOG" "
-	<b>Kernel for <code>$device</code> compiled succesfully!</b>
-	Total build time <b>$((SECONDS / 60))</b> minute(s) and <b>$((SECONDS % 60))</b> second(s) !
-	
-	#logs #$device "
-)
-done
+# Devices variable
+ZIPNAME="Kara-TEST"
+DEVICE="onclite"
+DEFCONFIG=onclite-perf_defconfig
+
+# EnvSetup
+KBUILD_BUILD_USER="PapiLer"
+KBUILD_BUILD_HOST=Laptop-Sangar
+export CHATID="-1001851989071"
+export KBUILD_BUILD_HOST KBUILD_BUILD_USER
+
+export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
+export CI_BRANCH=$DRONE_BRANCH
+
+# Check Kernel Version
+KERVER=$(make kernelversion)
+
+# Set a commit head
+COMMIT_HEAD=$(git log --oneline -1)
+
+##-----------------------------------------------------##
+
+clone() {
+	echo " "
+	msg "|| Cloning Clang ||"
+	git clone --depth=1 https://github.com/kdrag0n/proton-clang clang-llvm --no-tags --single-branch
+
+		# Toolchain Directory defaults to clang-llvm
+	TC_DIR=$KERNEL_DIR/clang-llvm
+
+	msg "|| Cloning Anykernel ||"
+	git clone --depth 1 --no-single-branch https://github.com/PapiLer/AnyKernel3.git -b master
+}
+
+##------------------------------------------------------##
+
+exports() {
+	export ARCH=arm64
+	export SUBARCH=arm64
+	export token=5748449722:AAF8DDlYIxcZ4fhyS-Nal7NurbicxNnAZBU
+
+	KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+	PATH=$TC_DIR/bin/:$PATH
+
+	export PATH KBUILD_COMPILER_STRING
+	export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
+	export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
+	PROCS=$(($(nproc --all) + 2))
+	export PROCS
+}
+
+##---------------------------------------------------------##
+
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$2" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+
+}
+
+##----------------------------------------------------------------##
+
+tg_post_build() {
+	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+	-F chat_id="$2"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$3 | <code>Build Number : </code><b>$DRONE_BUILD_NUMBER</b>"
+}
+
+##----------------------------------------------------------##
+
+build_kernel() {
+
+ 	tg_post_msg "<b>🔨 $KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>HEAD : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>" "$CHATID"
+ 	make O=out $DEFCONFIG CC="ccache clang" \ CLANG_TRIPLE="aarch64-linux-gnu-" \ CROSS_COMPILE="aarch64-linux-gnu-"\ CROSS_COMPILE_COMPAT="arm-linux-gnueabi-" \
+
+	msg "|| Started Compilation ||"
+	BUILD_START=$(date +"%s")
+	make -j$(nproc --all) O=out \
+                      ARCH=${ARCH}\
+                      CC="ccache clang" \
+	                CLANG_TRIPLE="aarch64-linux-gnu-" \
+	                CROSS_COMPILE="aarch64-linux-gnu-" \
+	                CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+                        -j$(nproc --all)
+	BUILD_END=$(date +"%s")
+	DIFF=$((BUILD_END - BUILD_START))
+
+		if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ]
+	    then
+	    	msg "|| Kernel successfully compiled ||"
+			gen_zip
+		else
+		tg_post_msg "<b>❌ Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>" "$CHATID"
+		fi
+
+}
+
+##--------------------------------------------------------------##
+
+gen_zip() {
+	msg "|| Zipping into a flashable zip ||"
+	cp "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
+	cd AnyKernel3 || exit
+	zip -r9 $ZIPNAME-$DEVICE-"$DRONE_BUILD_NUMBER" ./* -x .git README.md
+
+	## Prepare a final zip variable
+	ZIP_FINAL="$ZIPNAME-$DEVICE-$DRONE_BUILD_NUMBER.zip"
+	tg_post_build "$ZIP_FINAL" "$CHATID" "✅ Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+	cd ..
+	rm -rf AnyKernel3
+}
+
+clone
+exports
+build_kernel
+
+##----------------*****-----------------------------##
